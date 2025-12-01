b195199dff0ecce0db2811cefb8f3376942bda25
KONDAPURAMGURUPRASADREDDY <gurureddy405@gmail.com>
2025-12-01 15:16:34 +0530

    Updated app.py for Railway deploy

--- app.py
+++ app.py
@@ -231,4 +231,5 @@ def get_recommendation():
 # Run Server
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
